08b0b10b3d094c0975e9db4d663020079ece77ae
Veneta <venkireva80@gmail.com>
2023-11-09 16:38:10 +0200

    Add verb page

---
+++ app/pages/Verbs.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import streamlit as st
+import util_funcs
+from utils import verbs
+
+df = util_funcs.load_data()
+
+st.markdown('# Verbs')
+
+lollipop_fig = verbs.get_verb_lollipop(df)
+radar_fig_course = verbs.get_verb_radar_course(df)
+radar_fig_verb = verbs.get_verb_radar_verb(df)
+
+st.pyplot(lollipop_fig)
+
+with st.expander('Show the verb(s) associated with each course'):
+    st.plotly_chart(radar_fig_course, use_container_width=True)
+
+with st.expander('Show the course(s) associated with each verb'):
+    st.plotly_chart(radar_fig_verb, use_container_width=True)
---
+++ app/utils/verbs.py
@@ -0,0 +1,102 @@
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+
+
+def get_verb_lollipop(df):
+    fig, ax = plt.subplots(figsize=(16, 10), dpi=80)
+    ax.set_title(
+        'Interactions',
+        fontdict={'size': 22}
+    )
+    ax.set_xlabel(
+        'Count',
+        fontdict={
+            'size': 15
+        }
+    )
+    ax.set_yticks(range(len(df[['verb.id']].value_counts())))
+    ax.set_yticklabels(
+        df['verb.id'].drop_duplicates().tolist(),
+        fontdict={
+            'horizontalalignment': 'right',
+            'size': 15
+        }
+    )
+    ax.hlines(
+        y=range(len(df[['verb.id']].value_counts())),
+        xmin=0,
+        xmax=df[['verb.id']].value_counts().to_frame()[['count']].max(),
+        color='darkgray',
+        alpha=0.7,
+        linewidth=1,
+        linestyles='dashdot'
+    )
+    ax.scatter(
+        y=range(len(df[['verb.id']].value_counts())),
+        x=df[['verb.id']].value_counts().to_frame()[['count']],
+        s=75,
+        color='firebrick',
+        alpha=0.7
+    )
+    return fig
+
+
+def get_verb_radar_verb(df):
+    df_radar = (df
+                .groupby(['Course', 'verb.id'])['verb.id']
+                .count()
+                .unstack(fill_value=0)
+                .stack()
+                .reset_index()
+                .rename(columns={0: 'count'}))
+
+    fig = go.Figure()
+
+    for course in df_radar['Course'].drop_duplicates().tolist():
+        fig.add_trace(go.Scatterpolar(
+            r=df_radar[df_radar['Course'] == course]['count'].tolist(),
+            theta=df_radar['verb.id'].drop_duplicates().tolist(),
+            fill='toself',
+            name=course
+        ))
+
+    fig.update_layout(
+        polar=dict(
+            radialaxis=dict(
+                visible=True,
+            )),
+        showlegend=True
+    )
+
+    return fig
+
+
+def get_verb_radar_course(df):
+    df_radar = (df
+                .groupby(['verb.id', 'Course'])['Course']
+                .count()
+                .unstack(fill_value=0)
+                .stack()
+                .reset_index()
+                .rename(columns={0: 'count'}))
+
+    fig = go.Figure()
+
+    for verb in df_radar['verb.id'].drop_duplicates().tolist():
+        fig.add_trace(go.Scatterpolar(
+            r=df_radar[df_radar['verb.id'] == verb]['count'].tolist(),
+            theta=df_radar['Course'].drop_duplicates().tolist(),
+            fill='toself',
+            name=verb
+        ))
+
+    fig.update_layout(
+        polar=dict(
+            radialaxis=dict(
+                visible=True,
+            )),
+        showlegend=True
+    )
+
+    return fig
+
